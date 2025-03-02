a8598e3226e5d7d2c4b01342a138d8230f53f3c0
DrinkTheStars <131472013+Drink-The-Stars@users.noreply.github.com>
2025-03-02 11:55:45 -0500

    Add files via upload

---
+++ FUCKYOU/fuckyou.lua
@@ -0,0 +1,17 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Fuck You
+--- MOD_ID: FuckYou
+--- MOD_AUTHOR: [DrinkTheStars]
+--- MOD_DESCRIPTION: Changes the "Nope!" text from a failed Wheel of Fortune to something much more appropriate.
+--- PRIORITY: 0
+--- DISPLAY_NAME: Fuck You
+--- BADGE_COLOUR: d18017
+
+----------------------------------------------
+------------ MOD CODE ------------------------
+SMODS.Language {
+	key = 'en-us',
+	label = "English",
+	}
+----------------------------------------------
+------------ END MOD CODE --------------------
---
+++ FUCKYOU/localization/en-us.lua
@@ -0,0 +1,7 @@
+return {
+	misc = {
+	        dictionary={
+         	   k_nope_ex="Fuck You!",
+        	},
+     	},
+}
